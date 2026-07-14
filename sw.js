a609bfeb77c1719e2100d9b618b28b319e019708
loungebarveramar-arch <loungebarveramar@gmail.com>
2026-07-14 02:20:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,4 +3,3 @@ const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","ic
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.hostname.includes("script.google.com")){e.respondWith(fetch(e.request));return}e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
-
